ca6213296e7d296b28ea11f66c757e83a93429da
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-22 18:16:45 +0700

    more tests

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
+import childProcess from 'child_process';
 import test from 'ava';
 import fn from './';
 
 test(t => {
 	const ctor = fn();
+	const ctorNative = childProcess.ChildProcess;
+
 	t.is(ctor.name, 'ChildProcess');
 	t.is(typeof ctor.prototype.spawn, 'function');
+
+	if (typeof ctorNative === 'function') {
+		t.is(ctor, ctorNative);
+		t.is(ctor.prototype.spawn, ctorNative.prototype.spawn);
+	}
+
 	t.end();
 });
